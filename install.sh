328356605c2e61f429ace3949298d485aad700a5
tiktokdaoqingcheng <161429204+tiktokdaoqingcheng@users.noreply.github.com>
2024-02-27 15:48:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8737,13 +8737,13 @@ menu() {
     echoContent green "作者：tiktokdaoqingcheng"
     echoContent green "当前版本：v3.2.11"
     echoContent green "Github：https://github.com/tiktokdaoqingcheng/v2ray-agent"
-    echoContent green "描述：八合一共存脚本\c"
+    echoContent green "描述：刀倾城八合一共存脚本\c"
     showInstallStatus
     checkWgetShowProgress
     echoContent red "\n=========================== 推广区============================"
     echoContent red "                                              "
-    echoContent green "VPS选购攻略：https://www.v2ray-agent.com/archives/1679975663984"
-    echoContent green "年付10美金低价VPS AS4837：https://www.v2ray-agent.com/archives/racknerdtao-can-zheng-li-nian-fu-10mei-yuan"
+    echoContent green "助力tiktok出海：https://t.me/tiktokdqc"
+    echoContent green "原生ISP 美国 日本 泰国 台湾 越南多地区"
     echoContent red "=============================================================="
     if [[ -n "${coreInstallType}" ]]; then
         echoContent yellow "1.重新安装"
